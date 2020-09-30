bb401792374eca67877f68fde145c2e1db4fd6fe
578822358@qq.com <18351014554@163.com>
2020-09-30 15:30:10 +0800

    contract processing type

--- src/main/java/com/example/EAS/dto/deno.java
+++ src/main/java/com/example/EAS/dto/deno.java
@@ -5,16 +5,16 @@
 //public class deno {
 //    {
 //        "taxBill":{
-//        "bankAccount":"936070443288800", "bank":"中国工商银行", "taxQua":"一般纳税人", "unionBankId":
-//        "d90AAAFhGsvOi0kP", "unionBankNum":"102100000021", "taxNum":"8008208820"
-//    },"endDate":"2020-10-01", "num":"WEB.B-571DC06.05GJ.0001", "conName":"easWeb合同保存测试testOne", "creatorId":
-//        "35TWYYCCTbKX3WlrWJaDfBO33n8=", "orgId":"d90AAAAACWzM567U", "marketConArray":[],"grtRate":"7", "currencyId":
-//        "dfd38d11-00fd-1000-e000-1ebdc0a8100dDEB58FDC", "conDetailArray":[],"grtAmount":"8641975.23", "amount":
-//        "123456789.00", "contractNature":"直接合同", "contractWFStartType":"内部关联公司往来类", "exRate":"1", "deptId":
-//        "d90AAAAAB4zM567U", "conTypeId":"d90AAAAAbbqzcXde", "signDetailArray":[],"csName":"招标", "originalAmount":
-//        "123456789", "createTime":"2020-09-29", "partA":"d90AAAAAiKA9DrBt", "partB":
-//        "cI0GbS59Tq6vmN/MTHvq0zfGffw=", "personId":"+rVXdpSgQHuuTry+gScCGhO33n8=", "projectId":
-//        "d90AAAAATID55ekr", "startDate":"2020-09-29"
+//        "bankAccount":"11111", "bank":"111111111", "taxQua":"一般纳税人", "unionBankId":"d90AAAFhGs/Oi0kP", "unionBankNum":
+//        "102100000064", "taxNum":"111111"
+//    },"hygh":"aI7I/kJsQ5enIUQ81uzPRuzgeds=", "endDate":"2020-10-29", "num":"WEB.B-571DC06.01TD.0001", "conName":
+//        "test01", "orgId":"d90AAAAACWzM567U", "marketConArray":[],"grtRate":"0", "currencyId":
+//        "dfd38d11-00fd-1000-e000-1ebdc0a8100dDEB58FDC", "conDetailArray":[],"grtAmount":"0.00", "amount":
+//        "1.00", "contractNature":"1", "contractWFStartType":"内部关联公司往来类", "partC":
+//        "cI0GbS59Tq6vmN/MTHvq0zfGffw=", "exRate":"1", "deptId":"d90AAAAACWzM567U", "conTypeId":
+//        "d90AAAAAZcmzcXde", "signDetailArray":[],"csName":"招标", "originalAmount":"1", "createTime":"2020-09-30", "partA":
+//        "d90AAAAAq4k9DrBt", "partB":"lmrsIpCSShOPjzSBn6R8RzfGffw=", "personId":
+//        "7BN7tH/rRZ6bkDmegl84KhO33n8=", "projectId":"d90AAAAATIT55ekr", "startDate":"2020-09-16"
 //    }
 //
 //}
