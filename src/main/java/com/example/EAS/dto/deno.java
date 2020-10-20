736052cf2a73d722810f45b3ecdb3a5cf0424b72
admin <33345099@qq.com>
2020-10-20 14:08:05 +0800

    无文本添加期间保存

--- src/main/java/com/example/EAS/dto/deno.java
+++ src/main/java/com/example/EAS/dto/deno.java
@@ -1,33 +1,12 @@
-//package com.example.EAS.dto;
-//
-//import java.util.Date;
-//
-//public class deno {
-//
-//    {
-//        "taxerNum":"913301065832469250", "endDate":"2020-11-10", "unionBankId":"d90AAAFhGs3Oi0kP", "num":
-//        "WEB.B-571DC06.02QQ.0006", "conName":"赵华生测试合同保存testNo5", "taxerQua":"一般纳税人", "orgId":
-//        "d90AAAAACWzM567U", "marketConArray":[{
-//        "date":"2020-10-08T16:00:00.000Z", "amount":"6010510181316.50", "rate":"10", "remark":"没有备注", "content":"不可描述"
-//    },{
-//        "date":"2020-10-09T16:00:00.000Z", "amount":"12021020362633.00", "rate":"20", "remark":"没有备注", "content":"不可描述"
-//    },{
-//        "date":"2020-10-11T16:00:00.000Z", "amount":"42073571269215.50", "rate":"70", "remark":"没有备注", "content":"不可描述"
-//    }],"bank":"中国工商银行股份有限公司北京东铁匠营支行", "grtRate":"20", "currencyId":
-//        "dfd38d11-00fd-1000-e000-1ebdc0a8100dDEB58FDC", "conDetailArray":[{
-//        "detailInfo":"是否使用电子章", "description":"不可描述", "content":"是"
-//    },{
-//        "detailInfo":"备注", "description":"不可描述", "content":"测试"
-//    }],"bankAccount":"960901261985161", "grtAmount":"12021020362633.00", "amount":"60105101813165.00", "contractNature":
-//        "直接合同", "contractWFStartType":"内部关联公司往来类", "contractWFTypeId":"d90AAABf/yWLHwk2", "partC":
-//        "95kUDpwCTjmMxT5dDQB+XzfGffw=", "exRate":"1", "deptId":"d90AAAAAScPM567U", "conTypeId":
-//        "d90AAAAAbayzcXde", "signDetailArray":[{
-//        "totalAmount":"999999", "amount":"769230.00", "rate":"30", "description":"测试zhs", "detail":"赵华生测试劳务名称"
-//    },{
-//        "totalAmount":"888888", "amount":"683760.00", "rate":"30", "description":"测试zhs", "detail":"赵华生测试劳务名称two"
-//    }],"csName":"招标", "originalAmount":"60105101813165", "createTime":"2020-10-09", "partA":"d90AAAAAVT89DrBt", "partB":
-//        "95kUDpwCTjmMxT5dDQB+XzfGffw=", "personId":"d90AAAAAVjCA733t", "projectId":"d90AAAAATH755ekr", "startDate":
-//        "2020-10-09"
+package com.example.EAS.dto;
+
+public class deno {
+
+//    public static void main(String[] args) {
+//        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        LocalDateTime now = LocalDateTime.now().plusMonths(-1);
+//        String format = df.format(now);
+//        System.out.println(format);
 //    }
-//
-//}
+
+}
