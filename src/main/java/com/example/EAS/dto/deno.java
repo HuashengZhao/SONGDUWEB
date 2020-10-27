3c7529ab147c70771081d42d73fb73640bdaedce
578822358@qq.com <18351014554@163.com>
2020-10-27 10:16:58 +0800

    无文本期间更新

--- src/main/java/com/example/EAS/dto/deno.java
+++ src/main/java/com/example/EAS/dto/deno.java
@@ -8,20 +8,21 @@ public class deno {
 //    }
 
 //    {
-//        "bankAccount":"5337896524563977", "periodId":"d90AAAFjdDmCOIxM", "amount":"2487", "costedDeptId":
-//        "d90AAAAAN0nM567U", "useDepartmentId":"d90AAAAAN0nM567U", "isJT":"false", "bizDate":
-//        "2020-10-22T00:00:00", "unionBankId":"d90AAAFhKADOi0kP", "rateAmount":0.00, "applierId":
-//        "d90AAAFSKVuA733t", "costedCompanyId":"d90AAAAACWzM567U", "conTypeId":"d90AAAAAZcmzcXde", "orgId":
-//        "d90AAAAACWzM567U", "invoiceAmt":2487.00, "number":"WEB.B-010QT999.0500000267", "bank":
-//        "中国工商银行苏州分行", "originalAmount":"2487", "receiverType":"职员", "name":"张-测试无文本-1022-0001", "personId":
-//        "d90AAAAAWcaA733t", "id":"d90AAAFm4zQ9mlOI", "bgArray":[{
-//        "amount":1010.00, "expenseTypeId":"d90AAAAIjfl45LyU"
-//    },{
-//        "amount":425.00, "expenseTypeId":"d90AAABoI5B45LyU"
-//    },{
-//        "amount":1052.00, "expenseTypeId":"d90AAAAIjgZ45LyU"
-//    }],"capitalAmount":"贰仟肆佰捌拾柒元整"
+//        "taxerNum":"913301065832469250", "useDepartmentId":"d90AAAAAScPM567U", "isJT":"false", "bizDate":
+//        "2020-10-27T00:00:00", "unionBankId":"d90AAAFhGsvOi0kP", "rateAmount":0.00, "description":
+//        "00000000000000000000000000", "taxerQua":"小规模纳税人", "orgId":"d90AAAAACWzM567U", "number":
+//        "WEB.1111100000333", "bank":"中国工商银行股份有限公司北京通州支行新华分理处", "realSupplierId":
+//        "95kUDpwCTjmMxT5dDQB+XzfGffw=", "receiveUnitId":"95kUDpwCTjmMxT5dDQB+XzfGffw=", "id":
+//        "d90AAAFnGbA9mlOI", "attach":[{
+//        "FDescription":"WEB", "FSize":"22", "FCreatorId":"rOMpAbGrRB6E8gwRzwXZ+BO33n8=", "FRemotePath":
+//        "/2020/10/27/1603764532287/84f7d982-4697-4911-a12a-ae49b64afe3f.txt", "FName":"面试安排.txt", "FNumber":
+//        "2020102700000519"
+//    }],"capitalAmount":"贰万元整", "bankAccount":"3211655165651", "periodId":"d90AAAFjdDmCOIxM", "amount":
+//        "20000.00", "costedDeptId":"d90AAAAANM3M567U", "payContentTypeId":"d90AAAAbx3pt71CG", "applierId":
+//        "d90AAABhFn2A733t", "costedCompanyId":"d90AAAAAB8jM567U", "conTypeId":"d90AAAAAZcmzcXde", "invoiceAmt":
+//        1000000.00, "originalAmount":"20000.00", "receiverType":"供应商", "name":"测测测款项说明没有了", "bgArray":[{
+//        "amount":20000.00, "expenseTypeId":"d90AAABFJBt45LyU"
+//    }],"projectId":"d90AAAAATH755ekr"
 //    }
-
 
 }
